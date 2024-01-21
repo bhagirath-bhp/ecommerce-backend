93c77382aca3fc9c20696db3cffd38009d22d900
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2024-01-21 12:05:57 +0530

    add: role access to previous api

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post("/orders/add",isLoggedIn,addOrder)
 router.get("/orders/:userId",isLoggedIn,getAllOrdersForAUser)
-router.get("/admin/orders",isLoggedIn,getAllOrdersForAdmin)
+router.get("/admin/orders",isLoggedIn,checkRole('admin'),getAllOrdersForAdmin)
 router.get("/orders",isLoggedIn,success)
 router.get("/order/:id",isLoggedIn,getOrderDetails)
 
